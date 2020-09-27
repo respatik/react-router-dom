736c19e8b65f538a20d76fe05cb2d4b5dbfe1e71
respatik <respatik@gmail.com>
2020-09-28 01:46:14 +0700

    adding home page

--- src/App.js
+++ src/App.js
@@ -7,13 +7,19 @@ import Register from './pages/Register'
 import Uppercase from './pages/Uppercase'
 import BMIcalculator from './pages/BMIcalculator'
 import Todos from './pages/Todos'
+import Home from './pages/Home'
 
 function App() {
   return (
     <Router>
     <div className="App">
+      <h1>Halo, Selamat Datang</h1>
+      <Home />
       <Header />
       <Switch>
+      <Route path="/home">
+        <Home />
+      </Route>
       <Route path="/register">
         <Register />
       </Route>
@@ -28,7 +34,7 @@ function App() {
       </Route>
       </Switch>
 
-      <h1>Halo, Selamat Datang</h1>
+      
 
     </div>
     </Router>
--- src/pages/Header.js
+++ src/pages/Header.js
@@ -3,13 +3,12 @@ import {Link}  from 'react-router-dom'
 
 export default function Header() {
     return (
-        <nav className="navbar navbar-dark bg-primary">
             <div className="row col-12 d-flex justify-content-center text-white">
+            <Link to="/home" className="h3">Home</Link>
             <Link to="/register" className="h3">Register</Link>
             <Link to="/uppercase" className="h3">Uppercase</Link>
             <Link to="/bmicalculator" className="h3">BMI Calculator</Link>
             <Link to="/todos" className="h3">Todos</Link>
             </div>
-        </nav>
     )
 }
